4bc1b3bbe10e262ed292fc06d0810bf217668bf0
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2023-11-06 10:46:38 -0600

    Correcciones de indentación
    Se cambio la indentación de 2 a 4 en las vistas

--- vistas/layout.php
+++ vistas/layout.php
@@ -1,69 +1,73 @@
 <?php
-    if(!isset($_SESSION)){
-        session_start();
-    }
-    $auth = $_SESSION["login"] ?? false;
+if (!isset($_SESSION)) {
+  session_start();
+}
+$auth = $_SESSION["login"] ?? false;
 
-    if(!isset($esInicio)){
-        $esInicio = false;
-    }
+if (!isset($esInicio)) {
+  $esInicio = false;
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="preload" href="/build/css/app.css" as="style">
-    <link rel="stylesheet" href="/build/css/app.css">
-    <title>Bienes raices</title>
+  <link rel="preload" href="/build/css/app.css" as="style">
+  <link rel="stylesheet" href="/build/css/app.css">
+  <title>Bienes raices</title>
 </head>
+
 <body>
-    <header class="header <?php echo $esInicio ? "inicio" : ""; ?>">
-        <div class="contenedor contenido-header">
-            <div class="barra">
-                <a href="/">
-                    <img src="/build/img/logo.svg" alt="Logo">
-                </a>
+  <header class="header <?php echo $esInicio ? "inicio" : ""; ?>">
+    <div class="contenedor contenido-header">
+      <div class="barra">
+        <a href="/">
+          <img src="/build/img/logo.svg" alt="Logo">
+        </a>
 
-                <div class="mobile-menu">
-                    <img src="/build/img/barras.svg" alt="icono de menu responsive">
-                </div>
-
-                <div class="derecha">
-                    <img class="dark-mode-boton" src="/build/img/dark-mode.svg" alt="dark mode">
-                    <div class="navegacion">
-                        <a href="/nosotros">Nosotros</a>
-                        <a href="/propiedades">Propiedades</a>
-                        <a href="/blog">Blog</a>
-                        <a href="/contacto">Contacto</a>
-                        <?php if($auth){ ?>
-                            <a href="/logout">Cerrar Sesión</a>
-                        <?php } ?>
-                    </div>
-                </div>
-            </div>
-            <?php if($esInicio){ ?>
-                <h1>Venta de Casas y Departamentos Exclusivos de Lujo</h1>
-            <?php } ?>    
+        <div class="mobile-menu">
+          <img src="/build/img/barras.svg" alt="icono de menu responsive">
         </div>
-    </header>
 
-    <?php echo $contenido; ?>
-
-    <footer class="footer seccion">
-        <div class="contenedor contenido-footer">
-            <div class="navegacion">
-                <a href="/nosotros">Nostros</a>
-                <a href="/propiedades">Propiedades</a>
-                <a href="/blog">Blog</a>
-                <a href="/contacto">Contacto</a>
-            </div>
+        <div class="derecha">
+          <img class="dark-mode-boton" src="/build/img/dark-mode.svg" alt="dark mode">
+          <div class="navegacion">
+            <a href="/nosotros">Nosotros</a>
+            <a href="/propiedades">Propiedades</a>
+            <a href="/blog">Blog</a>
+            <a href="/contacto">Contacto</a>
+            <?php if ($auth) { ?>
+              <a href="/logout">Cerrar Sesión</a>
+            <?php } ?>
+          </div>
         </div>
-        <p class="copyright">Todos los derechos reservados <?php echo date("Y"); ?> &copy;</p>
-    </footer>
-    <script src="/build/js/bundle.min.js"></script>
+      </div>
+      <?php if ($esInicio) { ?>
+        <h1>Venta de Casas y Departamentos Exclusivos de Lujo</h1>
+      <?php } ?>
+    </div>
+  </header>
+
+  <?php echo $contenido; ?>
+
+  <footer class="footer seccion">
+    <div class="contenedor contenido-footer">
+      <div class="navegacion">
+        <a href="/nosotros">Nostros</a>
+        <a href="/propiedades">Propiedades</a>
+        <a href="/blog">Blog</a>
+        <a href="/contacto">Contacto</a>
+      </div>
+    </div>
+    <p class="copyright">Todos los derechos reservados <?php echo date("Y"); ?> &copy;</p>
+  </footer>
+  <script src="/build/js/bundle.min.js"></script>
 </body>
+
 </html>
--- vistas/login/restablecerContraseña.php
+++ vistas/login/restablecerContraseña.php
@@ -1,16 +1,16 @@
 <main class="contenedor seccion contenido-centrado">
-    <h1>Restablecer Contraseña</h1>
-    
-    <form class="formulario" method="POST">
-        <fieldset>
-            <legend>Contraseña</legend>
+  <h1>Restablecer Contraseña</h1>
 
-            <label for="password">Contraseña</label>
-            <input id="password" type="password" name="password" placeholder="Tu Password">
-            
-            <label for="password">Confirmar Contraseña</label>
-            <input id="c-password" type="password" name="c-password" placeholder="Tu Password">
-        </fieldset>
-        <input class="boton boton-verde" type="submit" value="Restablecer contraseña">
-    </form>
+  <form class="formulario" method="POST">
+    <fieldset>
+      <legend>Contraseña</legend>
+
+      <label for="password">Contraseña</label>
+      <input id="password" type="password" name="password" placeholder="Tu Password">
+
+      <label for="password">Confirmar Contraseña</label>
+      <input id="c-password" type="password" name="c-password" placeholder="Tu Password">
+    </fieldset>
+    <input class="boton boton-verde" type="submit" value="Restablecer contraseña">
+  </form>
 </main>
