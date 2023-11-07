7818617495bb038b2f99f7fe8942c190229608c6
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2023-11-07 04:19:01 -0600

    Hacer la vista de crear usuario

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . "/../includes/app.php";
 use MVC\Router;
 use Controlador\PaginasController;
+use Controlador\CtrlUsuario;
 
 $router = New Router();
 
@@ -11,5 +12,8 @@ $router->get("/propiedades",[PaginasController::class, "vistaPropiedades"]);
 $router->get("/propiedad",[PaginasController::class, "vistaPropiedad"]);
 $router->get("/blog",[PaginasController::class, "vistaBlogs"]);
 $router->get("/entrada",[PaginasController::class, "vistaEntrada"]);
+
+$router->get("/usuarios/crear",[CtrlUsuario::class, "vistaCrearUsuario"]);
+$router->post("/usuarios/crear",[CtrlUsuario::class, "crearUsuario"]);
 
 $router->comprobarRutas();
---
+++ vistas/usuarios/crear.php
@@ -0,0 +1,8 @@
+<main class="contenedor seccion">
+  <h1>Crear Usuario(a)</h1>
+  <form class="formulario" method="POST">
+    <?php include __DIR__ . "/formulario.php" ?>
+    <input type="submit" value="Crear Usuario(a)" class="boton boton-verde">
+  </form>
+  <a href="/admin" class="boton boton-amarillo">Volver</a>
+</main>
