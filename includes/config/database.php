42aee26b92ca3e6e6107ee38df70460db207a023
Carlos <carlosmay220@gmail.com>
2024-05-12 05:55:59 +0000

    Edit db

--- includes/config/database.php
+++ includes/config/database.php
@@ -2,7 +2,7 @@
 function conectarDB() : mysqli{
     $db = mysqli_connect(
         $_ENV['DB_HOST'] ?? 'localhost',
-        $_ENV['DB_USER'] ?? '', 
+        $_ENV['DB_USER'] ?? 'carlos-may', 
         $_ENV['DB_PASS'] ?? '12345678', 
         $_ENV['DB_NAME'] ?? 'carlos_may_db'
     );
